beb3e8adce02272e807b8849b0bac222a861a386
Yuferril <yuferril.yunos@gmail.com>
2022-12-05 21:00:01 +1100

    Done with lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const greeting = [];
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, message) {
+  for (let i = 0; i < names.length; i++) {
+    greeting.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`);
+    console.log(greeting);
+  }
+
+  return greeting;
+}
+
+let num = 0;
+
+function countDown(num){
+    while (num >= 0) {
+        console.log(num--);
+      }
+
+}
+
+countDown(10);
